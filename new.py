3c0b7ea2e99cf478637769e740c57fe7056439e6
bellobambo <bellobambo21@gmail.com>
2023-07-02 09:07:21 +0100

    second commit

--- new.py
+++ new.py
@@ -1,3 +1,18 @@
-print('My name is')
-for i in range(5):
-    print('Jimmy Five Times (' + str(i) + ')')
+def collatz(number):
+    if number % 2 == 0:  # If number is even
+        result = number // 2
+    else:  # If number is odd
+        result = 3 * number + 1
+
+    print(result)
+    return result
+
+def collatz_sequence():
+    try:
+        number = int(input("Enter an integer: "))
+        while number != 1:
+            number = collatz(number)
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+collatz_sequence()
