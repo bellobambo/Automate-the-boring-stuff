9b452580f8aa6e65cc2fc9bc9ffa49470b072150
bellobambo <bellobambo21@gmail.com>
2023-07-04 14:21:40 +0100

    new commit

--- new.py
+++ new.py
@@ -1,18 +1,11 @@
-def collatz(number):
-    if number % 2 == 0:  # If number is even
-        result = number // 2
-    else:  # If number is odd
-        result = 3 * number + 1
-
-    print(result)
-    return result
-
-def collatz_sequence():
-    try:
-        number = int(input("Enter an integer: "))
-        while number != 1:
-            number = collatz(number)
-    except ValueError:
-        print("Invalid input. Please enter an integer.")
-
-collatz_sequence()
+catNames = []
+while True:
+    print('Enter the name of cat ' + str(len(catNames) + 1) +
+      ' (Or enter nothing to stop.):')
+    name = input()
+    if name == '':
+        break
+    catNames = catNames + [name]  # list concatenation
+print('The cat names are:')
+for name in catNames:
+    print('  ' + name)
