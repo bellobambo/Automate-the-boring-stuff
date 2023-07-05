4f59f3dddd2b2e393d3c384e1c98d2a39934eb9b
bellobambo <bellobambo21@gmail.com>
2023-07-05 06:15:52 +0100

    dictionaries

--- new.py
+++ new.py
@@ -1,11 +1,26 @@
-catNames = []
-while True:
-    print('Enter the name of cat ' + str(len(catNames) + 1) +
-      ' (Or enter nothing to stop.):')
-    name = input()
-    if name == '':
-        break
-    catNames = catNames + [name]  # list concatenation
-print('The cat names are:')
-for name in catNames:
-    print('  ' + name)
+theBoard = {
+    'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+    'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+    'low-L': ' ', 'low-M': ' ', 'low-R': ' '
+}
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+turn = 'X'
+
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+
+printBoard(theBoard)
