94ee2d8df81021dcfcd2daffc34b0dfc452b493f
bellobambo <bellobambo21@gmail.com>
2023-07-08 18:43:29 +0100

    new commit

--- one.py
+++ one.py
@@ -1,22 +1,21 @@
-def isPhoneNumber(text):
-    if len(text) != 12:
-        return False
-    for i in range(0, 3):
-        if not text[i].isdecimal():
-            return False
-    if text[3] != '-':
-        return False
-    for i in range(4, 7):
-        if not text[i].isdecimal():
-            return False
-    if text[7] != '-':
-        return False
-    for i in range(8, 12):
-        if not text[i].isdecimal():
-            return False
-    return True
+import pyinputplus as pyip
+import random, time
 
-print('Is 415-555-4242 a phone number?')
-print(isPhoneNumber('415-555-4242'))
-print('Is Moshi moshi a phone number?')
-print(isPhoneNumber('Moshi moshi'))
+numberOfQuestions = 10
+correctAnswers = 0
+for questionNumber in range(numberOfQuestions):
+     # Pick two random numbers:
+    num1 = random.randint(0, 9)
+    num2 = random.randint(0, 9)
+
+    prompt = '#%s: %s x %s = ' % (questionNumber, num1, num2)
+      try:
+        # Right answers are handled by allowRegexes.
+        # Wrong answers are handled by blockRegexes, with a custom message.
+        pyip.inputStr(prompt, allowRegexes=['^%s$' % (num1 * num2)],
+                              blockRegexes=[('.*', 'Incorrect!')],
+                              timeout=8, limit=3)
+            except pyip.TimeoutException:
+        print('Out of time!')
+    except pyip.RetryLimitException:
+        print('Out of tries!')
